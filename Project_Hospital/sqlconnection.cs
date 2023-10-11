e19c2cfc1ce6193f25d8ff2d4d002ea7ea53048a
Mehmethan Bellek <bellekmehmethan@gmail.com>
2023-10-11 23:42:05 +0300

    sqlconnection

--- Project_Hospital/sqlconnection.cs
+++ Project_Hospital/sqlconnection.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Project_Hospital
 {
-    internal class sqlconnection
+    class sqlconnection
     {
+        public SqlConnection connection()
+        {
+            SqlConnection connection = new SqlConnection("Data Source=MEHMETHAN;Initial Catalog=HospitalProject;Integrated Security=True");
+            connection.Open();
+            return connection;
+        }
     }
 }
