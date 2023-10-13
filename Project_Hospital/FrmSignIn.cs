ed97a85cfdf2bc5eb9ba2f005281921109b1676c
Mehmethan Bellek <bellekmehmethan@gmail.com>
2023-10-13 23:54:40 +0300

    devam

--- Project_Hospital/FrmSignIn.cs
+++ Project_Hospital/FrmSignIn.cs
@@ -23,6 +23,16 @@ namespace Project_Hospital
         private void BtnSignIn_Click(object sender, EventArgs e)
         {
             SqlCommand command = new SqlCommand("insert into Tbl_patients (PatientName,PatientSurname,PatientTC,PatientMobile,PatientPassword,PatientGender) values (@p1,@p2,@p3,@p4,@p5,@p6)",connect.connection());
+            command.Parameters.AddWithValue("@p1", TxtName.Text);
+            command.Parameters.AddWithValue("@p2", TxtSurname.Text);
+            command.Parameters.AddWithValue("@p3", MskTc.Text);
+            command.Parameters.AddWithValue("@p4", MskMobile.Text);
+            command.Parameters.AddWithValue("@p5", TxtPassword.Text);
+            command.Parameters.AddWithValue("@p6", CmbGender.Text);
+            command.ExecuteNonQuery();
+            connect.connection().Close();
+            MessageBox.Show("Sign Up Successful. Your Password: " + TxtPassword.Text,"Information",MessageBoxButtons.OK,MessageBoxIcon.Information);
+
         }
     }
 }
